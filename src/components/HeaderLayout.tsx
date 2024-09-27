e9b3a61fae7708821ec20b43fbdb50e1d610ee24
Nurdin <mrsunnysummer@gmail.com>
2024-09-28 06:58:21 +0700

    style: define new color and change home pages layout

---
+++ src/components/HeaderLayout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react"
+import pky from "../assets/images/web/pky.png"
+export default function HeaderLayout({ children }: { children: ReactNode }) {
+    return (
+        <>
+            <div className="flex items-center col-span-4 p-8 mb-4 space-x-8 text-left bg-primary text-secondary rounded-xl">
+                <img className="h-48" src={pky} alt="" />
+                <div>
+                    <p>tanggal</p>
+                    <h1 className="my-6 text-4xl font-bold">
+                        Sistem Antrian Berbasis Web Dinas Pendidikan Kota
+                        Palangka Raya
+                    </h1>
+                    <p className="text-2xl">Semoga harimu menyenangkan!</p>
+                </div>
+            </div>
+            <main>{children}</main>
+        </>
+    )
+}
