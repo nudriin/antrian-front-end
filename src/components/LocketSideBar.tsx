b00ba9a29fb00acf283cca8bac53783431490969
Nurdin <mrsunnysummer@gmail.com>
2024-10-01 20:39:34 +0700

    update: change locket sidebar to be dynamic

--- src/components/LocketSideBar.tsx
+++ src/components/LocketSideBar.tsx
@@ -1,20 +1,10 @@
 import { NavLink } from "react-router-dom"
-import {
-    TbBoxMultiple1,
-    TbBoxMultiple2,
-    TbBoxMultiple3,
-    TbBoxMultiple4,
-    TbBoxMultiple5,
-    TbBoxMultiple6,
-    TbIcons,
-} from "react-icons/tb"
+import { TbIcons } from "react-icons/tb"
 import pky from "../assets/images/web/pky.png"
+import { useCallback, useEffect, useState } from "react"
+import { Locket } from "../types/locket"
 
 export default function LocketSideBar() {
-    const activeLink =
-        "flex items-center gap-3 px-4 py-3 font-bold font-rubik rounded-full bg-primary text-white"
-    const nonActiveLink =
-        "flex items-center gap-3 px-4 py-3 font-bold font-rubik hover:bg-primary hover:text-white hover:rounded-full"
     return (
         <div className="flex flex-col justify-between gap-4">
             <NavLink to="/">
@@ -34,96 +24,88 @@ export default function LocketSideBar() {
                     </NavLink>
                 </ul>
             </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/paud"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple1 size={25} />
-                        </li>
-                        <li className="">Loket PAUD</li>
-                    </NavLink>
-                </ul>
-            </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/ptk"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple2 size={25} />
-                        </li>
-                        <li className="">Loket PTK</li>
-                    </NavLink>
-                </ul>
-            </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/sd"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple3 size={25} />
-                        </li>
-                        <li className="">Loket SD</li>
-                    </NavLink>
-                </ul>
-            </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/smp"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple4 size={25} />
-                        </li>
-                        <li className="">Loket SMP</li>
-                    </NavLink>
-                </ul>
-            </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/umpeg"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple5 size={25} />
-                        </li>
-                        <li className="">Loket UMPEG</li>
-                    </NavLink>
-                </ul>
-            </div>
-            <div className="text-left">
-                <ul className="ml-2 mr-2">
-                    <NavLink
-                        to="/locket/keuangan"
-                        className={({ isActive }) =>
-                            isActive ? activeLink : nonActiveLink
-                        }
-                    >
-                        <li>
-                            <TbBoxMultiple6 size={25} />
-                        </li>
-                        <li className="">Loket Keuangan</li>
-                    </NavLink>
-                </ul>
-            </div>
+            <LocketLists />
         </div>
+    )
+}
+
+function LocketLists() {
+    const [locket, setLocket] = useState<Locket[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const activeLink =
+        "flex items-center gap-3 px-4 py-3 font-bold font-rubik rounded-full bg-primary text-white"
+    const nonActiveLink =
+        "flex items-center gap-3 px-4 py-3 font-bold font-rubik hover:bg-primary hover:text-white hover:rounded-full"
+
+    const getAllLocket = useCallback(async () => {
+        try {
+            const response = await fetch("/api/locket", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            const body = await response.json()
+
+            if (!body.errors) {
+                setLocket(() => body.data)
+                setLoading(false)
+            } else {
+                setLoading(false)
+                throw new Error(body.errors)
+            }
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
+    }, [])
+
+    useEffect(() => {
+        getAllLocket()
+    }, [getAllLocket])
+
+    return (
+        <>
+            {!loading &&
+                locket.map((value, index) => (
+                    <div className="text-left" key={index}>
+                        <LocketListSidebar
+                            activeLink={activeLink}
+                            nonActiveLink={nonActiveLink}
+                            locketName={value.name}
+                            linkTo={value.name}
+                        />
+                    </div>
+                ))}
+        </>
+    )
+}
+
+function LocketListSidebar({
+    activeLink,
+    nonActiveLink,
+    locketName,
+    linkTo,
+}: {
+    activeLink: string
+    nonActiveLink: string
+    locketName: string
+    linkTo: string
+}) {
+    return (
+        <ul className="ml-2 mr-2">
+            <NavLink
+                to={`/locket/${linkTo}`}
+                className={({ isActive }) =>
+                    isActive ? activeLink : nonActiveLink
+                }
+            >
+                <li>
+                    <TbIcons size={25} />
+                </li>
+                <li>Loket {locketName.toUpperCase()}</li>
+            </NavLink>
+        </ul>
     )
 }
