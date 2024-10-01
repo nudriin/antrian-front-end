f1e6401eaeb59b8066e0cc8e6f7367586fa234c3
Nurdin <mrsunnysummer@gmail.com>
2024-10-01 20:16:57 +0700

    update: change locket to be dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Queue from './pages/public/Queue';
-import Home from './pages/public/Home';
-import AddQueue from './pages/user/AddQueue';
-import LocketPaud from './pages/locket/LocketPaud';
-import LocketSmp from './pages/locket/LocketSmp';
-import LocketUmpeg from './pages/locket/LocketUmpeg';
-import LocketKeuangan from './pages/locket/LocketKeuangan';
-import Welcome from './pages/locket/Welcome';
-import Login from './pages/public/Login';
-import LocketTk from './pages/locket/LocketTk';
-import LocketSd from './pages/locket/LocketSd';
-import PrivateRoute from './components/PrivateRoute';
-import SignRoute from './components/SignRoute';
-import LocketPrivateRoute from './components/LocketPrivateRoute';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./App.css"
+import Queue from "./pages/public/Queue"
+import Home from "./pages/public/Home"
+import AddQueue from "./pages/user/AddQueue"
+import Login from "./pages/public/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import SignRoute from "./components/SignRoute"
+import LocketPrivateRoute from "./components/LocketPrivateRoute"
+import Locket from "./pages/locket/Locket"
+import Welcome from "./pages/locket/Welcome"
 
 function App() {
     return (
@@ -23,15 +18,7 @@ function App() {
 
                 <Route element={<LocketPrivateRoute />}>
                     <Route path="/locket" element={<Welcome />} />
-                    <Route path="/locket/paud" element={<LocketPaud />} />
-                    <Route path="/locket/ptk" element={<LocketTk />} />
-                    <Route path="/locket/sd" element={<LocketSd />} />
-                    <Route path="/locket/smp" element={<LocketSmp />} />
-                    <Route path="/locket/umpeg" element={<LocketUmpeg />} />
-                    <Route
-                        path="/locket/keuangan"
-                        element={<LocketKeuangan />}
-                    />
+                    <Route path="/locket/:locketName" element={<Locket />} />
                 </Route>
                 <Route element={<SignRoute />}>
                     <Route path="/login" element={<Login />} />
@@ -42,7 +29,7 @@ function App() {
                 </Route>
             </Routes>
         </BrowserRouter>
-    );
+    )
 }
 
-export default App;
+export default App
