6e58b2afb8698e0cac14f58f101504b98271dd90
Nurdin <mrsunnysummer@gmail.com>
2024-10-02 06:58:43 +0700

    feat: create locket update and move delete and update lokcet button to be a component

--- src/pages/admin/AdminLocket.tsx
+++ src/pages/admin/AdminLocket.tsx
@@ -8,24 +8,12 @@ import {
     CardTitle,
 } from "../../components/ui/card"
 import { Locket } from "../../types/locket"
-import { TbTrashXFilled } from "react-icons/tb"
-import { FaIcons, FaRegEdit } from "react-icons/fa"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import CreateLocketBtn from "../../components/CreateLocketBtn"
-import { useCookies } from "react-cookie"
-import { toast } from "../../hooks/use-toast"
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "../../components/ui/alert-dialog"
+
 import { BsEyeFill } from "react-icons/bs"
+import DeleteLocketBtn from "../../components/DeleteLocketBtn"
+import UpdateLocketBtn from "../../components/UpdateLocketBtn"
 
 export default function AdminLocket() {
     return (
@@ -77,44 +65,6 @@ function LocketsCards() {
 }
 
 function LocketsCard({ locket }: { locket: Locket }) {
-    const [loading, setLoading] = useState<boolean>(false)
-    const [cookie] = useCookies(["auth"])
-    const token = cookie.auth
-    const navigate = useNavigate()
-    const handleDelete = async (id: number) => {
-        try {
-            setLoading(true)
-            const response = await fetch(`/api/locket/${id}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            const body = await response.json()
-
-            if (!body.errors) {
-                setLoading(false)
-                toast({
-                    title: "Sukses",
-                    description: "Loket berhasil dihapus",
-                })
-                navigate(0)
-            } else {
-                setLoading(false)
-                throw new Error(body.errors)
-            }
-        } catch (error) {
-            setLoading(false)
-            console.log(error)
-            toast({
-                title: "Error",
-                description: `${error}`,
-                variant: "destructive",
-            })
-        }
-    }
-
     return (
         <Card className="h-full gap-4 rounded-2xl group border-primary/20 hover:cursor-pointer">
             <CardHeader>
@@ -130,44 +80,8 @@ function LocketsCard({ locket }: { locket: Locket }) {
                         <BsEyeFill className="text-secondary" />
                     </Link>
                 </Button>
-                <Button className="bg-secondary text-primary" size={"icon"}>
-                    <FaRegEdit />
-                </Button>
-                <AlertDialog>
-                    <AlertDialogTrigger asChild>
-                        <Button className="bg-red-500">
-                            <TbTrashXFilled />
-                        </Button>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle>
-                                Kamu yakin ingin menghapus loket ini?
-                            </AlertDialogTitle>
-                            <AlertDialogDescription>
-                                <span className="text-red-500">
-                                    Dengan menghapus loket ini, kamu tidak akan
-                                    bisa mengembalikannya lagi
-                                </span>
-                            </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                            <AlertDialogCancel>Batal</AlertDialogCancel>
-                            <AlertDialogAction
-                                disabled={loading}
-                                className="bg-red-500"
-                                onClick={() => {
-                                    handleDelete(locket.id)
-                                }}
-                            >
-                                Hapus{" "}
-                                {loading && (
-                                    <FaIcons className="animate-spin" />
-                                )}
-                            </AlertDialogAction>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
+                <UpdateLocketBtn id={locket.id} name={locket.name} />
+                <DeleteLocketBtn id={locket.id} />
             </CardFooter>
         </Card>
     )
