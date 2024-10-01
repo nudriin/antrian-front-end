dfa5dbefe7661ff81bf24b636c5340c263aed98e
Nurdin <mrsunnysummer@gmail.com>
2024-10-01 19:26:52 +0700

    update: implement date in header layout and update textToSpeech

--- src/components/HeaderLayout.tsx
+++ src/components/HeaderLayout.tsx
@@ -1,15 +1,29 @@
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
 import pky from "../assets/images/web/pky.png"
+import moment from "moment"
+import "moment/locale/id"
 export default function HeaderLayout({ children }: { children: ReactNode }) {
+    const [date, setDate] = useState<string>()
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const dateNow = new Date()
+            moment.locale("id")
+            const localDate = moment(dateNow).format("LLLL")
+            setDate(localDate)
+        }, 3000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    })
     return (
         <>
             <div className="flex items-center col-span-4 p-8 mb-4 space-x-8 text-left bg-white text-primary rounded-xl">
                 <img className="h-48" src={pky} alt="" />
                 <div>
-                    <p>tanggal</p>
+                    <p>{date}</p>
                     <h1 className="my-6 text-4xl font-bold">
-                        Sistem Antrian Berbasis Web Dinas Pendidikan Kota
-                        Palangka Raya
+                        Antri Helu Dinas Pendidikan Kota Palangka Raya
                     </h1>
                     <p className="text-2xl">Semoga harimu menyenangkan!</p>
                 </div>
--- src/helper/textToSpeech.tsx
+++ src/helper/textToSpeech.tsx
@@ -1,24 +1,26 @@
-import tingtung from '../assets/audio/tingtung.mp3';
+import tingtung from "../assets/audio/tingtung.mp3"
 export default function textToSpeech(text: string) {
-    const synth = window.speechSynthesis;
+    const synth = window.speechSynthesis
     try {
         const utterance: SpeechSynthesisUtterance =
-            new SpeechSynthesisUtterance(text);
-        const voices = synth.getVoices();
+            new SpeechSynthesisUtterance(text)
+        const voices = speechSynthesis.getVoices()
 
-        const voice =
-            voices.find((v) => v.name === 'Google Bahasa Indonesia') ?? null;
-        utterance.voice = voice;
+        const voice = voices.find((v) => v.name === "Google Bahasa Indonesia")
+        console.log(voice)
+        if (voice) {
+            utterance.voice = voice
 
-        utterance.rate = 0.9;
-        utterance.pitch = 1;
-        utterance.volume = 1;
-        const audio = new Audio(tingtung);
-        audio.play();
-        setTimeout(() => {
-            synth.speak(utterance);
-        }, 2400);
+            utterance.rate = 0.9
+            utterance.pitch = 1
+            utterance.volume = 1
+            const audio = new Audio(tingtung)
+            audio.play()
+            setTimeout(() => {
+                synth.speak(utterance)
+            }, 2400)
+        }
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
