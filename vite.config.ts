811259600ee3f5f9b9f6657914e7863143d30529
Nurdin <mrsunnysummer@gmail.com>
2024-11-04 00:53:29 +0700

    feat: create pwa, and update component ui shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,48 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Antri Helu",
+                short_name: "AntriHelu",
+                description: "",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/pwa-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
     server: {
         proxy: {
             "/api": {
