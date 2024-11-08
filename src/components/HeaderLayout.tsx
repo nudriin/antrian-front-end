bab0ef525d652696b2b95714261247b54514d1c2
Nurdin <mrsunnysummer@gmail.com>
2024-11-08 16:48:56 +0700

    style: remove clock from header layout

--- src/components/HeaderLayout.tsx
+++ src/components/HeaderLayout.tsx
@@ -1,22 +1,7 @@
-import { ReactNode, useEffect, useState } from "react"
+import { ReactNode } from "react"
 import pky from "../assets/images/web/pky.png"
-import moment from "moment/min/moment-with-locales"
-import "moment/locale/id"
 import { Link } from "react-router-dom"
 export default function HeaderLayout({ children }: { children: ReactNode }) {
-    const [date, setDate] = useState<string>()
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const dateNow = new Date()
-            moment.locale("id")
-            const localDate = moment(dateNow).format("LLLL")
-            setDate(localDate)
-        }, 3000)
-
-        return () => {
-            clearInterval(interval)
-        }
-    })
     return (
         <>
             <div className="items-center gap-2 p-4 mb-4 bg-white md:space-x-8 md:text-left md:p-8 md:flex text-primary rounded-xl">
@@ -28,7 +13,6 @@ export default function HeaderLayout({ children }: { children: ReactNode }) {
                     />
                 </Link>
                 <div className="">
-                    <p>{date}</p>
                     <h1 className="my-6 text-4xl font-bold">
                         Antri Helu Dinas Pendidikan Kota Palangka Raya
                     </h1>
